779bc860fa5ff6609439d9baa52e424a37935e22
lekan koku <kokulekan23@gmail.com>
2018-06-06 14:52:58 +0100

    authentication

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  MONOGO_URI: process.env.MONOGO_URI,
+  COOKIE_KEY: process.env.COOKIE_KEY
+};
