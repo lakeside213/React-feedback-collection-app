666e0b658e8239ea16d90492db220ae487ff94ee
lekan koku <kokulekan23@gmail.com>
2018-06-14 11:34:48 +0100

    app

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,6 @@ module.exports = {
   COOKIE_KEY: process.env.COOKIE_KEY,
   STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.sendGridKey,
   redirectDomain: process.env.redirectDomain,
   email: process.env.email,
   password: process.env.password
